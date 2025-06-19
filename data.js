1bf36d125e0374e21899a3098616df424f89db0c
elnataarq <elnata.arq@gmail.com>
2025-06-19 12:52:45 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,275 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_ap-kamlani_1",
+      "name": "Panorama_AP KAMLANI_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1822295881834108,
+        "pitch": 0.06865867605286269,
+        "fov": 1.2959776848255018
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.605242090306147,
+          "pitch": 0.25902469904604075,
+          "rotation": 0,
+          "target": "1-panorama_ap-kamlani_2"
+        },
+        {
+          "yaw": -3.0579357538455323,
+          "pitch": 0.17083192051659424,
+          "rotation": 0,
+          "target": "4-panorama_ap-kamlani_6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama_ap-kamlani_2",
+      "name": "Panorama_AP KAMLANI_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9692980556530202,
+        "pitch": 0.11981605350330682,
+        "fov": 1.2959776848255018
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.708274891133712,
+          "pitch": 0.2193117927099344,
+          "rotation": 0,
+          "target": "0-panorama_ap-kamlani_1"
+        },
+        {
+          "yaw": -1.3285276853131158,
+          "pitch": 0.2168387166571506,
+          "rotation": 0,
+          "target": "4-panorama_ap-kamlani_6"
+        },
+        {
+          "yaw": 1.4253906201759978,
+          "pitch": 0.23662519786867264,
+          "rotation": 0,
+          "target": "3-panorama_ap-kamlani_5"
+        },
+        {
+          "yaw": 2.942933778470022,
+          "pitch": 0.2867276984043645,
+          "rotation": 0,
+          "target": "2-panorama_ap-kamlani_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-panorama_ap-kamlani_4",
+      "name": "Panorama_AP KAMLANI_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.3458604095802311,
+        "pitch": 0.09071545090827371,
+        "fov": 1.2959776848255018
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.267081007892166,
+          "pitch": 0.33220480943071706,
+          "rotation": 0,
+          "target": "1-panorama_ap-kamlani_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-panorama_ap-kamlani_5",
+      "name": "Panorama_AP KAMLANI_5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.263544930433013,
+        "pitch": 0.2654840125679563,
+        "fov": 1.2959776848255018
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.168967795583555,
+          "pitch": 0.3220821338070081,
+          "rotation": 0,
+          "target": "1-panorama_ap-kamlani_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-panorama_ap-kamlani_6",
+      "name": "Panorama_AP KAMLANI_6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.013685878693092235,
+        "pitch": 0.137658314593196,
+        "fov": 1.2959776848255018
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8055339183707737,
+          "pitch": 0.30112865743245365,
+          "rotation": 0,
+          "target": "0-panorama_ap-kamlani_1"
+        },
+        {
+          "yaw": -1.627163987853649,
+          "pitch": 0.06369147575192358,
+          "rotation": 0,
+          "target": "5-panorama_ap-kamlani_7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-panorama_ap-kamlani_7",
+      "name": "Panorama_AP KAMLANI_7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6871048816816128,
+          "pitch": 0.41992678914259685,
+          "rotation": 0,
+          "target": "0-panorama_ap-kamlani_1"
+        },
+        {
+          "yaw": -1.715233756123105,
+          "pitch": 0.06431013549801001,
+          "rotation": 0,
+          "target": "4-panorama_ap-kamlani_6"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "AP_KAMLANI",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
